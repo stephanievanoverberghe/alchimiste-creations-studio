e324a1c879fc06693214c5a8177a64045136430c
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2026-03-13 10:36:50 +0100

    refactor: files services

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from '@/components/marketing/Hero';
 import { Problem } from '@/components/marketing/Problem';
 import { PortfolioPreview } from '@/components/marketing/PortfolioPreview';
 import { Process } from '@/components/marketing/Process';
-import { ServicesPreview } from '@/components/services';
+import { ServicesPreview } from '@/components/marketing/ServicesPreview';
 import { ValueProposition } from '@/components/marketing/ValueProposition';
 import { SocialProof } from '@/components/marketing/SocialProof';
 import { FinalCta } from '@/components/marketing/FinalCta';
--- src/components/services/index.ts
+++
@@ -1,1 +0,0 @@
-export { ServicesPreview } from './ServicesPreview';
